79bf9aceb567b04732f394998bad23a5ff8b0836
Hiroto9563 <88461646+Hiroto9563@users.noreply.github.com>
2024-10-13 22:51:55 -0500

    Add files via upload

---
+++ World of Gaming/Feedback.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("form");
+
+form.addEventListener('submit', (e) => {
+    if (!form.checkValidity()) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+    form.classList.add('was-validated');
+});
+
+
